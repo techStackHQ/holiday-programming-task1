cbacc71d331b319c53031fb1f696dbcb20759bde
Obinna <anatuluihemobinna@gmail.com>
2024-08-28 02:35:50 -0600

    version 2

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 console.log("i heard a rumor");
 console.log("That you danced");
+console.log("what is your name?");
+console.log("where are you from?");
+console.log("what is your favourite food?");
